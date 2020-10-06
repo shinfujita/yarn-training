147c0c0c668ebec1e869341aa9f489640f3ba678
shinfujita <shin4522@gmail.com>
2020-10-06 16:07:51 +0900

    2020年度版教材を反映

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 'use strict';
+// 2019年度版
+// const request = require('request');
+// request('https://www.google.com', (error, response, body) => {
+//     console.log(body);
+// });
+
+// 2020年度版
+const axios = require('axios');
+axios.get ('https://www.google.com').then(res => {
+    console.log(res.data);
+});
